e286eee469b623205c4ed8872c817a123efc700b
gbmcpe <gbmcpe1@gmail.com>
2026-02-03 17:37:08 -0600

    Basic Player Class

--- YahtzeeGame/YahtzeeGame/Player.cs
+++ YahtzeeGame/YahtzeeGame/Player.cs
@@ -8,7 +8,51 @@ namespace YahtzeeGame
 {
     internal class Player
     {
-        int DummyChange;
+
+
+        private int PlayerPos;
+        private string PlayerName;
+        private bool ComputerPlayer;
+        // private ScoreColumn PlayerScores;
+        public int SelectedScore;
+
+
+        //constructors
+        public Player(int Pos) 
+        { 
+            PlayerPos = Pos;
+            PlayerName = ($"Player {Pos}");
+            ComputerPlayer = false;
+        }
+
+        public Player(int Pos, string Name)
+        {
+            PlayerPos = Pos;
+            PlayerName = Name;
+            ComputerPlayer = false;
+        }
+
+        public Player(int Pos, string Name, bool Comp)
+        {
+            PlayerPos = Pos;
+            PlayerName = Name;
+            ComputerPlayer = Comp;
+
+        }
+
+
+        private void DisplayScore()
+        {
+            
+        }
+
+
+        private void UpdateScoreColumn()
+        {
+            
+        }
+
+
 
     }
 }
