8a2df4be999a6bf1b77a229dd0e3fd043eb21ac2
bradyevander098-bit <bradyevander098@gmail.com>
2026-02-09 17:52:19 -0600

    Dice Code

--- YahtzeeGame/YahtzeeGame/MainWindow.xaml.cs
+++ YahtzeeGame/YahtzeeGame/MainWindow.xaml.cs
@@ -24,6 +24,16 @@ namespace YahtzeeGame
         {
             InitializeComponent();
         }
+        private bool[] CheckDice()
+        {
+            bool[] dice = new bool[4];
+            dice[0] = cbDie1.IsChecked ?? false;
+            dice[1] = cbDie2.IsChecked ?? false;
+            dice[2] = cbDie3.IsChecked ?? false;
+            dice[3] = cbDie4.IsChecked ?? false;
+            dice[4] = cbDie5.IsChecked ?? false;
+            return dice;
+        }
 
         /// <summary>
         /// Closes Program.
@@ -33,6 +43,52 @@ namespace YahtzeeGame
         private void BtnClose_Click(object sender, RoutedEventArgs e)
         {
             Close();
+        }
+
+        private void BtnRollDice_Click(object sender, RoutedEventArgs e)
+        {
+            bool[] DiceState = CheckDice();
+
+            for(int c = 0; c <5; c++)
+            {
+                if (!DiceState[c])
+                {
+                    if (int.Parse(lblTimesRolled.ContentStringFormat) >3)
+                    {
+
+                        Random random = new Random();
+                        
+                        int DieValue = random.Next(1, 7);
+
+                        if(c == 0) 
+                        {
+                            cbDie1.ContentStringFormat = DieValue.ToString();
+                            Die1.Source = new BitmapImage(new Uri($@"Die{DieValue}.bmp", UriKind.Relative));
+                        }
+                        else if (c == 1)
+                        {
+                            cbDie2.ContentStringFormat = DieValue.ToString();
+                            Die2.Source = new BitmapImage(new Uri($@"Die{DieValue}.bmp", UriKind.Relative));
+                        }
+                        else if (c == 2)
+                        {
+                            cbDie3.ContentStringFormat = DieValue.ToString();
+                            Die3.Source = new BitmapImage(new Uri($@"Die{DieValue}.bmp", UriKind.Relative));
+                        }
+                        else if (c == 3)
+                        {
+                            cbDie4.ContentStringFormat = DieValue.ToString();
+                            Die4.Source = new BitmapImage(new Uri($@"Die{DieValue}.bmp", UriKind.Relative));
+                        }
+                        else if (c == 4)
+                        {
+                            cbDie5.ContentStringFormat = DieValue.ToString();
+                            Die5.Source = new BitmapImage(new Uri($@"Die{DieValue}.bmp", UriKind.Relative));
+                        }
+                    }
+
+                }
+            }
         }
     }
 }
