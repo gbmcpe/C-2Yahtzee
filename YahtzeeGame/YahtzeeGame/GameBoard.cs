fd3cc35fb542ce0437bdd50d690eac7ac65a12a0
B-45acp <beaubaker2000@gmail.com>
2026-02-10 14:26:54 -0600

    added notes to class
    note

--- YahtzeeGame/YahtzeeGame/GameBoard.cs
+++ YahtzeeGame/YahtzeeGame/GameBoard.cs
@@ -2,9 +2,18 @@
 /// <summary>
 /// Class Obejct Beau Is responsible for coding
 /// </summary>
+/// 
+
+
+/*
+ * player initialize.
+ * highscore tracking.
+ * test ui configurations.
+ */
 public class Class1
 {
 	public Class1()
 	{
+
 	}
 }
