11c70446f04f4c5c06ef6877efb64e413277d6da
gbmcpe <gbmcpe1@gmail.com>
2026-02-10 01:35:38 -0600

    Optimization. Regions. Documentation.

--- YahtzeeGame/YahtzeeGame/MainWindow.xaml.cs
+++ YahtzeeGame/YahtzeeGame/MainWindow.xaml.cs
@@ -24,22 +24,15 @@ namespace YahtzeeGame
         {
             InitializeComponent();
         }
-        private bool[] CheckDice()
-        {
-            bool[] dice = new bool[5];
-            dice[0] = cbDie1.IsChecked ?? false;
-            dice[1] = cbDie2.IsChecked ?? false;
-            dice[2] = cbDie3.IsChecked ?? false;
-            dice[3] = cbDie4.IsChecked ?? false;
-            dice[4] = cbDie5.IsChecked ?? false;
-            return dice;
-        }
+
+        #region Click Events
 
         /// <summary>
         /// Closes Program.
         /// </summary>
         /// <param name="sender"></param>
-        /// <param name="e"></param>
+        /// <param name="e"></param>'
+
         private void BtnClose_Click(object sender, RoutedEventArgs e)
         {
             Close();
@@ -48,77 +41,110 @@ namespace YahtzeeGame
         private void BtnRollDice_Click(object sender, RoutedEventArgs e)
         {
 
+            //Function that handles rolling the dice pool and displaying matching visuals.
 
-
-
-
+            //Counter for times rolled in one Turn. Starts at 3, and decreases by 1 every time the dice are rerolled.
             int counter = int.Parse(lblTimesRolled.ContentStringFormat);
 
-            lblTimesRolled.ContentStringFormat = (counter-1).ToString();
+            lblTimesRolled.ContentStringFormat = (counter - 1).ToString();
             lblTimesRolled.Content = (counter - 1).ToString();
 
+            //Gets the current input state.
             bool[] DiceState = CheckDice();
 
+            //RNG for dice.
             Random random = new Random();
 
-            for (int c = 0; c <5; c++)
+            //For loop to take us through each of the 5 dice sequentially.
+
+            for (int c = 0; c < 5; c++)
             {
-                if (DiceState[c]==false)
+                //If Statement Triggers if a die is unchecked. Checked Dice do not reroll.
+
+                if (DiceState[c] == false)
                 {
-
-                        
-
                     int DieValue = random.Next(1, 7);
 
-                    if (c == 0)
-                    {
-                        cbDie1.ContentStringFormat = DieValue.ToString();
+                    //Displays rolled die face. c = die face.
 
-                        Die1.Source = new BitmapImage(new Uri($@"{DieValue}Die.bmp", UriKind.Relative));
-
-                    }
-                        else if (c == 1)
-                        {
-                            cbDie2.ContentStringFormat = DieValue.ToString();
-                            Die2.Source = new BitmapImage(new Uri($@"{DieValue}Die.bmp", UriKind.Relative));
-                        }
-                        else if (c == 2)
-                        {
-                            cbDie3.ContentStringFormat = DieValue.ToString();
-                            Die3.Source = new BitmapImage(new Uri($@"{DieValue}Die.bmp", UriKind.Relative));
-                    }
-                        else if (c == 3)
-                        {
-                            cbDie4.ContentStringFormat = DieValue.ToString();
-                            Die4.Source = new BitmapImage(new Uri($@"{DieValue}Die.bmp", UriKind.Relative));
-                    }
-                        else if (c == 4)
-                        {
-                            cbDie5.ContentStringFormat = DieValue.ToString();
-                            Die5.Source = new BitmapImage(new Uri($@"{DieValue}Die.bmp", UriKind.Relative));
-                    }
+                    DisplayDice(c, DieValue);
 
                 }
             }
 
+            // If Statement fires once the roll counter hits 0, marking the end of a turn. 
+           
             if (int.Parse(lblTimesRolled.ContentStringFormat) == 0)
             {
-
                 TurnActivation(false);
-
             }
-
 
         }
 
         private void btnStart_Click(object sender, RoutedEventArgs e)
         {
+            //Upon starting, the start button is disabled, and the turn controls are activated.
             TurnActivation(true);
             btnStart.IsEnabled = false;
         }
 
+
+        private void BtnReset_Click(object sender, RoutedEventArgs e)
+        {
+            //Resets the Turn Space and Deactivates the Turn
+            Reset();
+            TurnActivation(false);
+
+        }
+
+        private void btnAbout_Click(object sender, RoutedEventArgs e)
+        {
+            //Program Information
+            MessageBox.Show("Yahtzee Version 0.1. Made By Marcus Cantrall, Bradye Vanderheyden,Connor Orton, Nicole Gonzalez Rodriguez and Beau Baker. ");
+        }
+        #endregion
+
+        #region Voids
+
+        private void Reset()
+        {
+            //Resets the dice images and Turn Roll Counter
+
+            Die1.Source = new BitmapImage(new Uri($@"1Die.bmp", UriKind.Relative));
+            Die2.Source = new BitmapImage(new Uri($@"2Die.bmp", UriKind.Relative));
+            Die3.Source = new BitmapImage(new Uri($@"3Die.bmp", UriKind.Relative));
+            Die4.Source = new BitmapImage(new Uri($@"4Die.bmp", UriKind.Relative));
+            Die5.Source = new BitmapImage(new Uri($@"5Die.bmp", UriKind.Relative));
+
+            lblTimesRolled.Content = "3";
+            lblTimesRolled.ContentStringFormat = "3";
+            btnStart.IsEnabled = true;
+
+        }
+
+        private void DisplayDice(int DicePos, int DiceValue)
+        {
+            //This method displays the results of dice rolls in the form dynamically, so that the code is reused.
+
+            //Currently we hold the dice's numeric value in the ContentStringFormat property.
+            //Change when Dice Class is fully implemented?
+
+            //Find Controls
+            CheckBox DieCheck = (CheckBox)this.FindName($"cbDie{DicePos + 1}");
+            Image DieImage = (Image)this.FindName($"Die{DicePos + 1}");
+
+            //Record Die Value
+            DieCheck.ContentStringFormat = DiceValue.ToString();
+            //Set new Die Image
+            DieImage.Source = new BitmapImage(new Uri($@"{DiceValue}Die.bmp", UriKind.Relative));
+        }
+
         private void TurnActivation(bool b)
         {
+
+            // Taking in a Bool Argument b, this method enables the turn controls
+            // if b = true, and disables them if b = false.
+
             cbDie1.IsEnabled = b;
             cbDie2.IsEnabled = b;
             cbDie3.IsEnabled = b;
@@ -135,32 +161,28 @@ namespace YahtzeeGame
 
         }
 
-        private void Reset()
+        #endregion
+
+        #region value-returning
+        private bool[] CheckDice()
         {
+            //This function packages the control data from the dice.
+            //It packages the checked state of the dice into an array
+            //that shows what dice are selected.
 
-
-            Die1.Source = new BitmapImage(new Uri($@"1Die.bmp", UriKind.Relative));
-            Die2.Source = new BitmapImage(new Uri($@"2Die.bmp", UriKind.Relative));
-            Die3.Source = new BitmapImage(new Uri($@"3Die.bmp", UriKind.Relative));
-            Die4.Source = new BitmapImage(new Uri($@"4Die.bmp", UriKind.Relative));
-            Die5.Source = new BitmapImage(new Uri($@"5Die.bmp", UriKind.Relative));
-
-            lblRollCount.Content = "3";
-            lblRollCount.ContentStringFormat = "3";
-            btnStart.IsEnabled = true;
-
+            bool[] dice = new bool[5];
+            dice[0] = cbDie1.IsChecked ?? false;
+            dice[1] = cbDie2.IsChecked ?? false;
+            dice[2] = cbDie3.IsChecked ?? false;
+            dice[3] = cbDie4.IsChecked ?? false;
+            dice[4] = cbDie5.IsChecked ?? false;
+            return dice;
         }
 
-        private void BtnReset_Click(object sender, RoutedEventArgs e)
-        {
-            Reset();
-            TurnActivation(false);
+        #endregion
 
-        }
 
-        private void btnAbout_Click(object sender, RoutedEventArgs e)
-        {
-            MessageBox.Show("Yahtzee Version 0.1. Made By Marcus Cantrall, Bradye Vanderheyden,Connor Orton, Nicole Gonzalez Rodriguez and Beau Baker. ");
-        }
+
+
     }
 }
