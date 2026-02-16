29a29e86af6d1ac82ff64835447b8d02ba6c0cf2
gbmcpe <gbmcpe1@gmail.com>
2026-02-16 11:51:06 -0600

    Second Time

--- YahtzeeGame/YahtzeeGame/Game.cs
+++ YahtzeeGame/YahtzeeGame/Game.cs
@@ -1,22 +1,8 @@
 ﻿using System;
-using System.CodeDom;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace YahtzeeGame
+public class Class1
 {
-    public class Game
-    {
-
-
-       public List<Player> Players;
-        public Dice Dice;
-        public int Round;
-        public int Turn;
-        public int Initiative;
-       
-
-    }
+	public Class1()
+	{
+	}
 }
